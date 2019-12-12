090260e4a30a7065d42174635178de741bd87af6
Igor Cavalcante <igorcavlim@gmail.com>
2019-12-12 01:47:30 -0300

    AuthService na pagina do qrcode

--- src/app/pages/qrcode/qrcode.page.ts
+++ src/app/pages/qrcode/qrcode.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/services/auth/auth.service";
 
 @Component({
-  selector: 'app-qrcode',
-  templateUrl: './qrcode.page.html',
-  styleUrls: ['./qrcode.page.scss'],
+    selector: "app-qrcode",
+    templateUrl: "./qrcode.page.html",
+    styleUrls: ["./qrcode.page.scss"],
 })
 export class QrcodePage implements OnInit {
 
-  constructor() { }
+    private user: { logged: boolean, data: any };
+    public ready = false;
 
-  ngOnInit() {
-  }
+    constructor(
+        private authService: AuthService,
+    ) { }
+
+    ngOnInit() {
+        this.authService.updateUserData();
+        this.authService.user.subscribe((user) => {
+            this.user = user;
+            this.ready = true;
+        });
+    }
 
 }
